f580fee1e02c057b14f2cbea4bfafcd54097b411
jocquej <jocquej@jupiter2.(none)>
2014-10-30 16:46:56 -0400

    Data update

--- Konane/data/hillClimber/attrAndData/glue.sh
+++ Konane/data/hillClimber/attrAndData/glue.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 for file in ../attr-*;
 do
+    ending=${file:8}
     if [ ! -e "./attrAndData-$ending" ]; then
         cat "$file" "../data-$ending" >> "./attrAndData-$ending"
     fi
